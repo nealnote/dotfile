c7da2a1028b6de6588ec972e7400c92785351345
yanyaoer <yanyaoer@gmail.com>
2012-06-13 10:22:32 +0800

    update runme.sh & readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
+About
+---
+只需要 vim 配置的可以删去 `runme.sh` 里面 `init shell` 部分再执行
+
+Plugin
+---
+[旧文档](http://fouland.com/vim/dotvimrc.md) 里有一部分插件的说明，待更新
+
 Install
 ---
-
 	./runme.sh
--- runme.sh
+++ runme.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# [refence](https://github.com/lepture/dotfiles/blob/master/bootstrap.sh)
 
 cd $(dirname "$0")
 
@@ -17,8 +18,23 @@ link vim
 link vimrc
 vim +BundleInstall +qall
 
+echo "init python env ..."
+if which pip > /dev/null; then
+	echo "pip already installed"
+else
+	sudo easy_install pip
+fi
+sudo pip install flake8
 
 echo "init shell >>>"
 link bashrc
-link zshrc
 link tmux.conf
+link zshrc
+if [ ! -d ~/.oh-my-zsh ]; then
+	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+if which zsh > /dev/null; then
+	echo "chsh -s `which zsh`"
+else
+	echo "install zsh"
+fi
